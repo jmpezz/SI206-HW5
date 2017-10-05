7bba34cdf339e316a6f1e763d1ad728398ddaa14
Julia Pezzullo <jmpezz@umich.edu>
2017-10-05 18:46:42 -0400

    final push

--- SI206-HW5.py
+++ SI206-HW5.py
@@ -1,1 +1,11 @@
 import re
+
+file = open('regex_sum_32076.txt')
+lines = file.read()
+sum_list = []
+
+count_num = re.findall('[0-9]+', lines)
+for number in count_num:
+	sum_list.append(int(number))
+
+print(sum(sum_list))
